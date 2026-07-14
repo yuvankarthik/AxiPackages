aa2906a8196cf5a9909af96ff8c25a5c5431103d
Karthik Thirumoorthi <karthikjune90@gmail.com>
2026-07-14 13:10:07 +0530

    Commit 2
    Commit AxiData 2

--- AxInsTestPackage/Structures/Postgre/Scripts/Test_Script.sql
+++ AxInsTestPackage/Structures/Postgre/Scripts/Test_Script.sql
@@ -1,9 +1,9 @@
 <<
-CREATE TABLE UsersTest (UserID INT PRIMARY KEY,FullName VARCHAR(100),Email VARCHAR(100))
+CREATE TABLE UsersTest1 (UserID INT PRIMARY KEY,FullName VARCHAR(100),Email VARCHAR(100))
 >>
 
 <<
-CREATE TABLE Products (ProductID INT PRIMARY KEY,ProductName VARCHAR(50),Price DECIMAL(10, 2),StockQuantity INT)
+CREATE TABLE Products1 (ProductID INT PRIMARY KEY,ProductName VARCHAR(50),Price DECIMAL(10, 2),StockQuantity INT)
 >>
 
 
