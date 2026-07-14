55036c3e10a4f7a18ca0f1d47220b8018c5fa523
Karthik Thirumoorthi <karthikjune90@gmail.com>
2026-07-14 10:10:02 +0530

    AxInsTestPackage
    Commit / 14072026

---
+++ AxInsTestPackage/Structures/Postgre/Scripts/Test_Script.sql
@@ -0,0 +1,11 @@
+<<
+CREATE TABLE UsersTest (UserID INT PRIMARY KEY,FullName VARCHAR(100),Email VARCHAR(100))
+>>
+
+<<
+CREATE TABLE Products (ProductID INT PRIMARY KEY,ProductName VARCHAR(50),Price DECIMAL(10, 2),StockQuantity INT)
+>>
+
+
+
+
